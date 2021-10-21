4ebbba6f5524d295f9099cde3691af08f871edeb
Marc Demoustier <marc.demoustier@epita.fr>
2021-10-21 15:30:17 +0200

    update DemixrDataset class with unique output instead of multiple outputs

--- demixr/data/dataset.py
+++ demixr/data/dataset.py
@@ -6,22 +6,25 @@ import glob
 import numpy as np
 
 class DemixrDataset(torch.utils.data.Dataset):
-    def __init__(self, path):
+    def __init__(self, path, input_file='mixture', output_file='vocals'):
         self.path = path
+
+        self.input_file = input_file
+        self.output_file = output_file
 
         self.folders = sorted(glob.glob(os.path.join(self.path, '*')))
         self.len = len(self.folders)
-
-        self.labels = np.asarray(['mixture', 'drums', 'bass', 'other', 'vocals'])
 
 
     def __len__(self):
         return self.len
 
 
+    def __build_path__(self, idx, label):
+        return os.path.join(self.folders[idx], f'{label}.wav')
+
+
     def __getitem__(self, idx):
-        mixes = []
-        for label in self.labels:
-            mixes.append(torchaudio.load(os.path.join(self.folders[idx], f"{label}.wav")))
-        print(mixes)
-        return None
+        x, _ = torchaudio.load(self.__build_path__(idx, self.input_file))
+        y, _ = torchaudio.load(self.__build_path__(idx, self.output_file))
+        return x, y
