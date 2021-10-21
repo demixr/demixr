201c1c51ed3543749e995f6febc96e1b3f19efaa
Alexandre DEISS <alexandre.deiss@epita.fr>
2021-10-21 16:55:44 +0200

    Add the DataLoader and split in train, val and test datasets

--- demixr/data/dataset.py
+++ demixr/data/dataset.py
@@ -5,6 +5,9 @@ import glob
 
 import numpy as np
 
+TRAIN_SPLIT = 0.8
+VALIDATION_SPLIT = 0.2
+
 class DemixrDataset(torch.utils.data.Dataset):
     def __init__(self, path, input_file='mixture', output_file='vocals'):
         self.path = path
@@ -28,3 +31,31 @@ class DemixrDataset(torch.utils.data.Dataset):
         x, _ = torchaudio.load(self.__build_path__(idx, self.input_file))
         y, _ = torchaudio.load(self.__build_path__(idx, self.output_file))
         return x, y
+
+
+def create_dataloaders(data_path, output_file='vocals'):
+    dataset = DemixrDataset(os.path.join(data_path, 'train'), output_file=output_file)
+    test_dataset = DemixrDataset(os.path.join(data_path, 'test'), output_file=output_file)
+
+    lengths = [round(len(dataset) * split) for split in [TRAIN_SPLIT, VALIDATION_SPLIT]]
+    train_dataset, val_dataset = torch.utils.data.random_split(dataset, lengths=lengths)
+
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=1,
+        shuffle=True,
+    )
+
+    val_dataloader = torch.utils.data.DataLoader(
+        val_dataset,
+        batch_size=1,
+        shuffle=True,
+    )
+
+    test_dataloader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=1,
+        shuffle=False
+    )
+
+    return train_dataloader, val_dataloader, test_dataloader
