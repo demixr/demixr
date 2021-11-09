f2c11b21b312f0c20d79fa983c439f6fa432435f
Guillaume Merle <guillaume.mrle@gmail.com>
2021-11-09 16:15:55 +0100

    Update dataset paths getter

--- demixr/utils.py
+++ demixr/utils.py
@@ -1,24 +1,13 @@
 import os
 import glob
 
-def create_musdbhq_dict(dataset):
-    subsets = list()
 
-    for subset in ["train", "test"]:
-        tracks = glob.glob(os.path.join(dataset, subset, "*"))
-        samples = list()
-
-        # Go through tracks
-        for track_folder in sorted(tracks):
-            # Skip track if mixture is already written, assuming this track is done already
-            song = dict()
-            for stem in ["mix", "bass", "drums", "other", "vocals"]:
-                filename = stem if stem != "mix" else "mixture"
-                audio_path = os.path.join(track_folder, filename + ".wav")
-                song[stem] = audio_path
-
-            samples.append(song)
-
-        subsets.append(samples)
-
-    return subsets
+def get_dataset_paths(path):
+    return {
+        subset: [
+            os.path.join(song_folder, stem + ".wav")
+            for stem in ["mixture", "bass", "drums", "other", "vocals"]
+            for song_folder in sorted(glob.glob(os.path.join(path, subset, "*")))
+        ]
+        for subset in ["train", "test"]
+    }
